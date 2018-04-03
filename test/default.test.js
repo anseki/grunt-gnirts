5462611c834ee0a70a26597e4793a710345f9bf1
anseki <AnSeki.aff@gmail.com>
2018-04-03 11:07:44 +0900

    Apply template

--- test/default.test.js
+++ test/default.test.js
@@ -16,12 +16,13 @@ const expect = require('chai').expect,
   path = require('path'),
 
   FIXTURES_DIR_PATH = path.resolve(__dirname, 'fixtures'),
-  ALL_CONTENTS = ['content-1.html', 'content-2.html']
+  CONTENTS = ['content-1.html', 'content-2.html']
     .map(fileName => require('fs').readFileSync(
       path.join(FIXTURES_DIR_PATH, fileName), {encoding: 'utf8'}))
     .join(grunt.util.linefeed),
+  RES_METHOD = `${CONTENTS}<mangle>`,
   OUTPUT_PATH = 'path/to/output',
-  R_METHODS = `${ALL_CONTENTS}<mangle>`;
+  LIB_NAME = 'gnirts';
 
 function resetAll() {
   gnirts.mangle.resetHistory();
@@ -31,7 +32,7 @@ function resetAll() {
 function runTask(done, options, files) {
   let error;
   grunt.initConfig({
-    gnirts: {
+    [LIB_NAME]: {
       test: {
         options,
         files: files || [{
@@ -47,10 +48,23 @@ function runTask(done, options, files) {
   grunt.task.start({asyncDone: true});
 }
 
-grunt.registerTask('default', ['gnirts:test']);
+grunt.registerTask('default', [`${LIB_NAME}:test`]);
 sinon.stub(grunt.file, 'write');
 
 describe('implements a basic flow as file based plugin', () => {
+
+  it('should accept contents from all source files', done => {
+    mangleThrow = false;
+    resetAll();
+    runTask(
+      () => {
+        expect(gnirts.mangle.calledOnceWithExactly(CONTENTS)).to.be.true;
+        expect(grunt.file.write.calledOnceWithExactly(OUTPUT_PATH, RES_METHOD)).to.be.true;
+
+        done();
+      }
+    );
+  });
 
   it('should skip process if no file is input', done => {
     mangleThrow = false;
@@ -70,33 +84,17 @@ describe('implements a basic flow as file based plugin', () => {
     );
   });
 
-  it('should accept contents from all source files', done => {
-    mangleThrow = false;
-    resetAll();
-    runTask(
-      () => {
-        expect(gnirts.mangle.calledOnceWithExactly(ALL_CONTENTS)).to.be.true;
-        expect(grunt.file.write.calledOnceWithExactly(
-          OUTPUT_PATH, R_METHODS)).to.be.true;
-
-        done();
-      }
-    );
-  });
-
 });
 
 describe('mangle()', () => {
 
   it('should return processed value by method', done => {
     mangleThrow = false;
-
     resetAll();
     runTask(
       () => {
-        expect(gnirts.mangle.calledOnceWithExactly(ALL_CONTENTS)).to.be.true;
-        expect(grunt.file.write.calledOnceWithExactly(
-          OUTPUT_PATH, R_METHODS)).to.be.true;
+        expect(gnirts.mangle.calledOnceWithExactly(CONTENTS)).to.be.true;
+        expect(grunt.file.write.calledOnceWithExactly(OUTPUT_PATH, RES_METHOD)).to.be.true;
 
         done();
       }
@@ -105,28 +103,25 @@ describe('mangle()', () => {
 
   it('should throw an error if mangle failed', done => {
     mangleThrow = false;
-
     resetAll();
     runTask(
       error => {
+        expect(gnirts.mangle.calledOnceWithExactly(CONTENTS)).to.be.true;
+        expect(grunt.file.write.calledOnceWithExactly(OUTPUT_PATH, RES_METHOD)).to.be.true;
         expect(error).to.be.undefined;
-        expect(gnirts.mangle.calledOnce).to.be.true;
-        expect(grunt.file.write.calledOnceWithExactly(
-          OUTPUT_PATH, R_METHODS)).to.be.true;
 
         // Throws
         mangleThrow = true;
-
         resetAll();
         runTask(
           error => {
+            expect(gnirts.mangle.calledOnceWithExactly(CONTENTS)).to.be.true;
+            expect(grunt.file.write.notCalled).to.be.true;
             // expect(error instanceof Error).to.be.true;
             // expect(error).to.be.an('error', ERR_MSG);
             // These above don't work well...
             // https://github.com/chaijs/chai/issues/930
             expect(error.toString()).to.equal(`Error: ${ERR_MSG}`);
-            expect(gnirts.mangle.calledOnce).to.be.true;
-            expect(grunt.file.write.notCalled).to.be.true;
 
             done();
           }
